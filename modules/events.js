7249c39b9a5d28225f62ace2678f6d6a3fe308ae
NeonSpectrum <youngskymann@gmail.com>
2018-06-02 00:34:51 +0800

    must fix the bug pausing music but not 0 users

--- modules/events.js
+++ modules/events.js
@@ -21,7 +21,7 @@ bot.on('voiceStateUpdate', async (oldMember, newMember) => {
 
     msg = `**${oldMember.user.username}** has disconnected from **${bot.channels.get(oldMember.voiceChannelID).name}**`
 
-    if (newMember.guild.channels.get(oldMember.voiceChannelID).members.filter(s => !s.user.bot).size == 0) music.pause()
+    if (bot.channels.get(oldMember.voiceChannelID).members.filter(s => !s.user.bot).size == 0) music.pause()
 
     if (member.roleid) {
       oldMember.roles.remove(member.roleid).then(() => {
@@ -33,7 +33,7 @@ bot.on('voiceStateUpdate', async (oldMember, newMember) => {
 
     msg = `**${newMember.user.username}** has connected to **${bot.channels.get(newMember.voiceChannelID).name}**`
 
-    if (newMember.guild.channels.get(newMember.voiceChannelID).members.filter(s => !s.user.bot).size == 1) music.resume()
+    if (bot.channels.get(newMember.voiceChannelID).members.filter(s => !s.user.bot).size == 1) music.resume()
 
     if (config.music.roles[newMember.voiceChannelID]) {
       newMember.roles.add(config.music.roles[newMember.voiceChannelID]).then(() => {
