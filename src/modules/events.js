6523c18167ff8095d74774c36b16b525012aa8df
NeonSpectrum <youngskymann@gmail.com>
2018-08-30 19:57:22 +0800

    remove ausvin from voice notif

--- src/modules/events.js
+++ src/modules/events.js
@@ -9,7 +9,7 @@ const members = bot.events
 var Events = {}
 
 Events.voiceStateUpdate = async (oldMember, newMember) => {
-  if (newMember.user.bot) return
+  if (newMember.user.bot || newMember.user.id == '348126827186749440') return
   if (!members[newMember.user.id]) {
     members[newMember.user.id] = {
       roleid: null
@@ -23,15 +23,9 @@ Events.voiceStateUpdate = async (oldMember, newMember) => {
   if (oldMember.voiceChannelID !== null && newMember.voiceChannelID === null) {
     let music = new Music(oldMember)
 
-    msg = `**${oldMember.user.username}** has disconnected from **${
-      bot.channels.get(oldMember.voiceChannelID).name
-    }**`
+    msg = `**${oldMember.user.username}** has disconnected from **${bot.channels.get(oldMember.voiceChannelID).name}**`
 
-    if (
-      bot.channels
-        .get(oldMember.voiceChannelID)
-        .members.filter(s => !s.user.bot).size === 0
-    ) {
+    if (bot.channels.get(oldMember.voiceChannelID).members.filter(s => !s.user.bot).size === 0) {
       music.pause()
     }
 
@@ -43,21 +37,12 @@ Events.voiceStateUpdate = async (oldMember, newMember) => {
         })
         .catch(() => {})
     }
-  } else if (
-    oldMember.voiceChannelID === null &&
-    newMember.voiceChannelID !== null
-  ) {
+  } else if (oldMember.voiceChannelID === null && newMember.voiceChannelID !== null) {
     let music = new Music(newMember)
 
-    msg = `**${newMember.user.username}** has connected to **${
-      bot.channels.get(newMember.voiceChannelID).name
-    }**`
+    msg = `**${newMember.user.username}** has connected to **${bot.channels.get(newMember.voiceChannelID).name}**`
 
-    if (
-      bot.channels
-        .get(newMember.voiceChannelID)
-        .members.filter(s => !s.user.bot).size === 1
-    ) {
+    if (bot.channels.get(newMember.voiceChannelID).members.filter(s => !s.user.bot).size === 1) {
       music.resume()
     }
 
@@ -91,15 +76,9 @@ Events.voiceStateUpdate = async (oldMember, newMember) => {
         $.embed()
           .setAuthor(
             'Voice Presence Update',
-            `https://cdn.discordapp.com/avatars/${bot.user.id}/${
-              bot.user.avatar
-            }.png?size=16`
+            `https://cdn.discordapp.com/avatars/${bot.user.id}/${bot.user.avatar}.png?size=16`
           )
-          .setDescription(
-            `\`${moment().format(
-              'YYYY-MM-DD hh:mm:ss A'
-            )}\`:bust_in_silhouette:${msg}.`
-          )
+          .setDescription(`\`${moment().format('YYYY-MM-DD hh:mm:ss A')}\`:bust_in_silhouette:${msg}.`)
       )
     }
   }
@@ -111,57 +90,34 @@ Events.presenceUpdate = (oldMember, newMember) => {
   const config = $.getServerConfig(newMember.guild.id)
   var msg
   if (oldMember.presence.status !== newMember.presence.status) {
-    msg = `**${newMember.user.username}** is now **${
-      newMember.presence.status
-    }**`
+    msg = `**${newMember.user.username}** is now **${newMember.presence.status}**`
   } else if (oldMember.presence.activity !== newMember.presence.activity) {
     msg = `**${newMember.user.username}** is now ${
-      newMember.presence.activity
-        ? newMember.presence.activity.type.toLowerCase()
-        : 'playing'
-    } **${
-      !newMember.presence.activity
-        ? 'nothing'
-        : newMember.presence.activity.name
-    }**`
+      newMember.presence.activity ? newMember.presence.activity.type.toLowerCase() : 'playing'
+    } **${!newMember.presence.activity ? 'nothing' : newMember.presence.activity.name}**`
   }
   if (msg && bot.channels.get(config.channel.log)) {
     bot.channels.get(config.channel.log).send(
       $.embed()
         .setAuthor(
           'User Presence Update',
-          `https://cdn.discordapp.com/avatars/${bot.user.id}/${
-            bot.user.avatar
-          }.png?size=16`
+          `https://cdn.discordapp.com/avatars/${bot.user.id}/${bot.user.avatar}.png?size=16`
         )
-        .setDescription(
-          `\`${moment().format(
-            'YYYY-MM-DD hh:mm:ss A'
-          )}\`:bust_in_silhouette:${msg}.`
-        )
+        .setDescription(`\`${moment().format('YYYY-MM-DD hh:mm:ss A')}\`:bust_in_silhouette:${msg}.`)
     )
   }
 }
 
 Events.guildMemberAdd = member => {
   var config = $.getServerConfig(member.guild.id)
-  var channel = member.guild.channels.find(
-    x => x.rawPosition === 0 && x.type === 'text'
-  )
+  var channel = member.guild.channels.find(x => x.rawPosition === 0 && x.type === 'text')
 
   bot.channels
     .get(channel.id)
     .send(
       $.embed()
-        .setAuthor(
-          'New Member',
-          `https://cdn.discordapp.com/avatars/${bot.user.id}/${
-            bot.user.avatar
-          }.png?size=16`
-        )
-        .setDescription(
-          `Welcome to ${member.guild.name}, ${member.user.toString()}!`
-        )
+        .setAuthor('New Member', `https://cdn.discordapp.com/avatars/${bot.user.id}/${bot.user.avatar}.png?size=16`)
+        .setDescription(`Welcome to ${member.guild.name}, ${member.user.toString()}!`)
     )
     .then(s =>
       s
@@ -177,35 +133,22 @@ Events.guildMemberAdd = member => {
       $.embed()
         .setAuthor(
           'Guild Member Update',
-          `https://cdn.discordapp.com/avatars/${bot.user.id}/${
-            bot.user.avatar
-          }.png?size=16`
+          `https://cdn.discordapp.com/avatars/${bot.user.id}/${bot.user.avatar}.png?size=16`
         )
-        .setDescription(
-          `\`${moment().format('YYYY-MM-DD hh:mm:ss A')}\` ${
-            member.user.username
-          } joined the server.`
-        )
+        .setDescription(`\`${moment().format('YYYY-MM-DD hh:mm:ss A')}\` ${member.user.username} joined the server.`)
     )
   }
 }
 
 Events.guildMemberRemove = member => {
   var config = $.getServerConfig(member.guild.id)
-  var channel = member.guild.channels.find(
-    x => x.rawPosition === 0 && x.type === 'text'
-  )
+  var channel = member.guild.channels.find(x => x.rawPosition === 0 && x.type === 'text')
 
   bot.channels
     .get(channel.id)
     .send(
       $.embed()
-        .setAuthor(
-          'Member Left',
-          `https://cdn.discordapp.com/avatars/${bot.user.id}/${
-            bot.user.avatar
-          }.png?size=16`
-        )
+        .setAuthor('Member Left', `https://cdn.discordapp.com/avatars/${bot.user.id}/${bot.user.avatar}.png?size=16`)
         .setDescription(`${member.user.tag} left the server!`)
     )
     .then(s =>
@@ -220,15 +163,9 @@ Events.guildMemberRemove = member => {
       $.embed()
         .setAuthor(
           'Guild Member Update',
-          `https://cdn.discordapp.com/avatars/${bot.user.id}/${
-            bot.user.avatar
-          }.png?size=16`
+          `https://cdn.discordapp.com/avatars/${bot.user.id}/${bot.user.avatar}.png?size=16`
         )
-        .setDescription(
-          `\`${moment().format('YYYY-MM-DD hh:mm:ss A')}\` ${
-            member.user.username
-          } left the server.`
-        )
+        .setDescription(`\`${moment().format('YYYY-MM-DD hh:mm:ss A')}\` ${member.user.username} left the server.`)
     )
   }
 }
