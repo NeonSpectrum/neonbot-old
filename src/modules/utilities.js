9f4f0018d6c93ea4aa6a9d84550d5add649a8568
NeonSpectrum <youngskymann@gmail.com>
2019-04-20 18:33:37 +0000

    Update utilities.js

--- src/modules/utilities.js
+++ src/modules/utilities.js
@@ -157,7 +157,7 @@ Utilities.prototype.sms = async function(args) {
   } catch (err) {
     if (err.code == 21211) {
       msg.edit(embed().addField('Status:', 'Invalid phone number.'))
-    } else 
+    } else {
       msg.edit(embed().addField('Status:', 'Sending failed.'))
       $.warn('SMS', err)
     }
